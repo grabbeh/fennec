e0276042c0e13d998be5069a67e7b360b453e663
Michael <mbg@outlook.com>
2014-08-12 13:32:26 +0100

    Update authentication.js

--- client/angular/authentication.js
+++ client/angular/authentication.js
@@ -44,6 +44,7 @@ angular.module('app')
                 $http.get('/api/logout')
                     .success(function(){
                         $rootScope.user = false;
+                        $rootScope.menuModal = false;
                         $location.path('/');
                     })
             }
