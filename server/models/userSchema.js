fd200242e3f5eaae24f289889fbb9cefe4779fc2
Michael <mbg@outlook.com>
2014-08-13 10:16:41 +0100

    Update userSchema.js

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose')
 
 var User = new Schema({
 	portfolios: Array,
-    entity: String,
+        entity: String,
+        favourites: Array,
 	_id: String,
 	username: String,
 	hash: String,
