ee7d0290e2898f34493133a2ad6d621ddf925d11
Michael <mbg@outlook.com>
2014-08-13 09:40:46 +0100

    Update controllers.js

--- client/angular/controllers.js
+++ client/angular/controllers.js
@@ -110,7 +110,7 @@ angular.module('app')
             $.showModal = function(trademark){
                 $rootScope.modal = true;
                 trademarkModal.deactivate();
-                trademarkModal.activate({ trademark: trademark, user: user });
+                trademarkModal.activate({ trademark: trademark, user: $rootScope.user });
             };
             
             $.expiryFormValid = function(){
