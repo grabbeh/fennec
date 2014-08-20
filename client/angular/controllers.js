bfbc8ebb0ebaba685d5bfb4319ca7a238a4866fc
Michael <mbg@outlook.com>
2014-08-20 16:08:15 +0100

    Update controllers.js

--- client/angular/controllers.js
+++ client/angular/controllers.js
@@ -31,7 +31,7 @@ angular.module('app')
                 $scope.message = "";
                 if (marks.length > 0){
                     $.result = "Use the 'R' symbol for the following:";
-                    $.classes = $filter('extractClasses')(marks);
+                    $.classes = $filter('addSpecifications')($filter('extractClasses')(marks));
                 }
                 else {
                     $.result = "Use the 'TM' symbol";
@@ -51,7 +51,7 @@ angular.module('app')
                 $scope.message = "";
                 if (marks.length > 0){
                     $.result = "Use the 'R' symbol for the following";
-                    $.classes = $filter('extractClasses')(marks);
+                    $.classes = $filter('addSpecifications')($filter('extractClasses')(marks));
                 }
                 else {
                     $.result = "Use the 'TM' symbol";
