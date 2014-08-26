3902fbbd1a0757f6f088ad65fcf2870f7567eb8b
Michael <mbg@outlook.com>
2014-08-26 09:55:03 +0100

    Update authentication.js

--- client/angular/authentication.js
+++ client/angular/authentication.js
@@ -72,13 +72,11 @@ angular.module('app')
                     .success(function(res){
                         $window.sessionStorage.token = res.token;
                         $rootScope.user = true;
-                        if (pathService.existingPath){
-                            console.log(pathService.returnPath())
-                            $location.path(pathService.returnPath());
+                        if (pathService.returnPath() === undefined){
+                            $location.path('/select-portfolio');
                         }
                         else {
-                            console.log("No path")
-                            $location.path('/select-portfolio');
+                            $location.path(pathService.returnPath());
                         }
                     })
                     .error(function(err){
