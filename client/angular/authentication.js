33f714199f70ffcfe937612dbcb5022f721c7b37
Michael <mbg@outlook.com>
2014-08-26 09:38:58 +0100

    Update authentication.js

--- client/angular/authentication.js
+++ client/angular/authentication.js
@@ -73,7 +73,6 @@ angular.module('app')
                         $window.sessionStorage.token = res.token;
                         $rootScope.user = true;
                         if (pathService.existingPath){
-                            console.log(pathService.returnPath())
                             $location.path(pathService.returnPath());
                         }
                         else {
