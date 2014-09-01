8e710c87600874bf28af3aedee762a9abb0b21c2
Michael <mbg@outlook.com>
2014-09-01 12:29:48 +0100

    Update controllers.js

--- client/angular/controllers.js
+++ client/angular/controllers.js
@@ -2,7 +2,6 @@ angular.module('app')
 	    .controller('favouritesCtrl', ['$scope', '$rootScope', 'favourites', 'user', 'trademarkService', 'editTrademarkModal', function($scope, $rootScope, favourites, user, trademarkService, editTrademarkModal){
 	        var $ = $scope;
         	$.favourites = favourites;
-        	console.log($.favourites);
 	        $.user = user;
 	        $.activeTrademark = favourites[0];
 	        $.activateTrademark = function(trademark){
