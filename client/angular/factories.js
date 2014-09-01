3301306dbf886fa1b276f633cf6bc5bba84beb4d
Michael <mbg@outlook.com>
2014-09-01 10:40:31 +0100

    Update factories.js

--- client/angular/factories.js
+++ client/angular/factories.js
@@ -93,6 +93,7 @@ angular.module('app')
         },
         getUser: function(){
             return $http.get('/api/getUser').then(function(response){
+            	console.log(response.data);
                 return response.data;
             });
         },
