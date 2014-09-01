0ab671b825c16df2b1b348b278f39679e21b8243
Michael <mbg@outlook.com>
2014-09-01 10:35:27 +0100

    Update controllers.js

--- client/angular/controllers.js
+++ client/angular/controllers.js
@@ -679,7 +679,7 @@ angular.module('app')
                     }
                 });
             }
-        console.log("Ctrl " + user);       
+        console.log(user);       
         $.showModal = function(trademark){
             $rootScope.modal = true;
             trademarkModal.deactivate();
