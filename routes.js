81df4dac0b8e66cf18a75604f38a643e8533ee42
Michael <mbg@outlook.com>
2014-09-15 18:03:42 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -19,75 +19,60 @@ module.exports = function(app, x){
 
 	app.get('/download/downloadTrademarks', main.downloadTrademarks);
 
-	// Elasticsearch 
-	// 
+	// Elasticsearch  
 	app.post('/api/search', x, main.search);
 
 	app.get('/api/searchTrademarks/:portfolio', x, main.searchTrademarks);
 
 	// Country data (ISO, coordinates etc)
-
 	app.get('/api/countryData', x, world.countryData);
 
 	// Provide geojson for group of marks
-
 	app.get('/api/world/:portfolio/:group', x, world.worldForGroup);
 
 	// Filtered world on basis of given list of marks
-
 	app.post('/api/world/:portfolio', x, world.worldForListOfMarks);
 
 	// world on basis of country
-
 	app.get('/api/world/:portfolio', x, world.worldForCountry);
 
 	// Country-limited trade marks
-
 	app.get('/api/country/:portfolio/:country', x, country.marksForCountry);
 	app.post('/api/country/:portfolio/:country', x, country.filterMarksForCountry);
 
 	// Get group of marks
-
 	app.get('/api/trademarks/:portfolio/:group', x, group.groupOfMarks);
 
 	// Edit individual trade marks
-
 	app.get('/api/trademark/:id', x, main.getTrademark);
 	app.post('/api/trademark', x, main.addTrademark);
 	app.put('/api/trademark/:id', x, main.amendTrademark);
 	app.delete('/api/trademark/:id', x, main.deleteTrademark);
 
 	// List of marks
-
 	app.get('/api/listOfMarks/:portfolio', x, group.listOfMarks);
 
 	// Retrieve favourite marks
-
 	app.get('/api/favourites/:portfolio', x, group.favourites);
 
 	// Edit portfolio
-
 	app.post('/api/editGroup/:portfolio/:mark', x, group.editGroupOfMarks);
 	app.post('/api/addLogoToGroup/:portfolio/:mark', x, group.addLogoToGroup);
 	app.post('/api/editMarksInCountry/:portfolio', x, country.editMarksInCountry);
 
 	// Expiry dates
-
 	app.get('/api/expirydates/:portfolio', x, expiry.expiriesForGroup);
 	app.get('/api/expirydates/:portfolio/:country', x, expiry.expiriesForCountry);
 	app.get('/api/expiriesForYear/:portfolio', x, expiry.expiriesForYear);
 
 	// Notifications
-
 	app.get('/api/notifications', notification.unreadNotifications);
 	app.post('/api/notifications', notification.updateNotification);
 
 	// Activities
-
 	app.get('/api/activities/:portfolio', activity.findActivities);
 
 	// Users
-
 	app.post('/api/addUser', user.addUser);
 	app.post('/server/login', user.logIn);
 	app.get('/api/isAdmin', user.isAdmin);
@@ -100,27 +85,23 @@ module.exports = function(app, x){
 
 
 	// Passwords
-
 	app.post('/api/updatePassword', user.updatePassword);
 	app.post('/server/requestPasswordReset', user.requestPasswordReset);
 	app.post('/server/passwordReset/:id', user.resetPassword);
 
 	// Account creation
-
 	app.post('/api/createAccount', user.createAccount);
 
 	// Spreadsheet upload
-
 	app.post('/api/spreadsheet', x, spreadsheet.processExcel);
 
 	// Image upload
-
 	app.post('/api/upload', x, image.uploadImage);
 
 	// Message sending 
-
 	app.post('/server/processMessage', contact.processMessage)
 
+	// Serve .html file if route not matched
 	app.get('*', function(req, res){
 	  res.sendfile(__dirname + '/client/views/index.html');
 	});
