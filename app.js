b4b36d7093e1802b6873225a46dd4b5e19fd4ab9
Michael <mbg@outlook.com>
2014-09-20 10:17:04 +0100

    Update to structure

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ var express = require('express')
   , expressJwt = require('express-jwt')
   , https = require('https')
   , http = require('http')
-  , fs = require('fs')
   , multipart = require('connect-multiparty')
-  , secret = require('config/jwt-secret')
-  , db = require('config/paid-db')
-  , job = require('routes/agenda')
+  , secret = require('./server/config/jwt-secret')
+  , ssl = require('./server/config/ssl')
+  , db = require('./server/config/paid-db')
+  , job = require('./server/routes/agenda')
   , app = express();
-
-app.use('/api', expressJwt({secret: secret }));
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
@@ -21,42 +19,31 @@ app.all('*', function(req, res, next) {
     next();
 });
 
+app.use('/api', expressJwt({secret: secret }));
 app.use(bodyParser());
 app.use(multipart());
 app.use(serveStatic(__dirname + '/client'));
 
 mongoose.connect(db, function(err){
     if (err) { console.log(err); throw new Error(err.stack);}
-  });
+});
 
 // Agenda
-
 job.setUpAgenda(db);
 
 // middleware
-
 function x(req, res, next) {
   if (req.user) { next(); } 
   else { res.status(401).send(); }
 }
 
 // Routes
-
-require('./routes')(app, x);
-
-/*
-var options = {
-  key: fs.readFileSync('config/domain.pem'),
-  cert: fs.readFileSync('config/main.pem'),
-  ca: [fs.readFileSync('config/intermediate.pem')]
-};*/
+require('./server/routes')(app, x);
 
 // Development port
-
 http.createServer(app).listen(2002);
 
 // Production port
-
-//https.createServer(options, app).listen(2003);
+//https.createServer(ssl, app).listen(2003);
 
 
---
+++ server/routes.js
@@ -0,0 +1,115 @@
+var world = require('./routes/world')
+, path = require('path')
+, country = require('./routes/country')
+, group = require('./routes/group')
+, main = require('./routes/main')
+, user = require('./routes/users')
+, image = require('./routes/images')
+, expiry = require('./routes/expiry')
+, activity = require('./routes/activity')
+, notification = require('./routes/notification')
+, spreadsheet = require('./routes/excel-process')
+, contact = require('./routes/contact');
+
+module.exports = function(app, x){
+
+	app.get('/', function(req, res){
+		res.sendfile('./client/views/index.html');
+	});
+	
+	// Download trade marks based on query parameter "portfolio"
+	app.get('/download/downloadTrademarks', main.downloadTrademarks);
+
+	// Elasticsearch  
+	app.post('/api/search', x, main.search);
+
+	// General portfolio search function based on query parameters "portfolio", "group" and "country"
+	app.get('/api/searchTrademarks/:portfolio', x, main.searchTrademarks);
+
+	// Country data (ISO, coordinates etc) - optionally accepts query parameter in which case returns
+	// country data limited to countries where there are trade marks
+	app.get('/api/countryData', x, world.countryData);
+
+	// Provide geojson for group of marks
+	app.get('/api/world/:portfolio/:group', x, world.worldForGroup);
+
+	// Filtered world on basis of given list of marks
+	app.post('/api/world/:portfolio', x, world.worldForListOfMarks);
+
+	// geojson of world for given country (ISO code)
+	app.get('/api/world/:portfolio', x, world.worldForCountry);
+
+	// Country-limited trade marks
+	// Gets a list of all trade marks for a given portfolio in a given country (ISO code)
+	app.get('/api/country/:portfolio/:country', x, country.marksForCountry);
+	// In addition to the above, accepts list of trade marks and provides only those marks in a portfolio
+	app.post('/api/country/:portfolio/:country', x, country.filterMarksForCountry);
+
+	// Get group of marks
+	app.get('/api/trademarks/:portfolio/:group', x, group.groupOfMarks);
+
+	// Edit individual trade marks
+	app.get('/api/trademark/:id', x, main.getTrademark);
+	app.post('/api/trademark', x, main.addTrademark);
+	app.put('/api/trademark/:id', x, main.amendTrademark);
+	app.delete('/api/trademark/:id', x, main.deleteTrademark);
+
+	// Returns basic list of marks for portfolio - accepts optional query for ISO code in
+	// which case returns list of marks for given country
+	app.get('/api/listOfMarks/:portfolio', x, group.listOfMarks);
+
+	// Retrieve favourite marks
+	app.get('/api/favourites/:portfolio', x, group.favourites);
+
+	// Edit portfolio
+	app.post('/api/editGroup/:portfolio/:mark', x, group.editGroupOfMarks);
+	app.post('/api/addLogoToGroup/:portfolio/:mark', x, group.addLogoToGroup);
+	app.post('/api/editMarksInCountry/:portfolio', x, country.editMarksInCountry);
+
+	// Expiry dates
+	app.get('/api/expirydates/:portfolio', x, expiry.expiriesForGroup);
+	app.get('/api/expirydates/:portfolio/:country', x, expiry.expiriesForCountry);
+	app.get('/api/expiriesForYear/:portfolio', x, expiry.expiriesForYear);
+
+	// Notifications
+	app.get('/api/notifications', notification.unreadNotifications);
+	app.post('/api/notifications', notification.updateNotification);
+
+	// Activities
+	app.get('/api/activities/:portfolio', activity.findActivities);
+
+	// Users
+	app.post('/server/login', user.logIn);
+
+	// Routes for querying logged in user
+	app.get('/api/isAdmin', user.isAdmin);
+	app.get('/api/isUser', user.isUser);
+	app.get('/api/getUser', user.getUser);
+
+	app.post('/api/addUser', user.addUser);
+
+	app.put('/api/users/:id', user.updateUser);
+	app.get('/api/users', user.allUsers);
+	app.del('/api/users/:id', user.deleteUser);
+	
+	app.post('/api/createAccount', user.createAccount);
+
+	// Passwords (including reset)
+	app.post('/api/updatePassword', user.updatePassword);
+	app.post('/server/requestPasswordReset', user.requestPasswordReset);
+	app.post('/server/passwordReset/:id', user.resetPassword);
+
+	// Spreadsheet upload
+	app.post('/api/spreadsheet', x, spreadsheet.processExcel);
+
+	// Image upload
+	app.post('/api/upload', x, image.uploadImage);
+
+	// Message sending 
+	app.post('/server/processMessage', contact.processMessage);
+
+	// Serve .html file if route not matched
+	app.get('*', function(req, res){
+		res.sendfile('./client/views/index.html');
+	});
+};
