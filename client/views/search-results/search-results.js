6db1ed8835c726be560661b9fec677df5851b104
Michael <mbg@outlook.com>
2014-09-23 16:35:46 +0100

    Update search-results.js

--- client/views/search-results/search-results.js
+++ client/views/search-results/search-results.js
@@ -1,8 +1,7 @@
 angular.module('app')
 .directive('mgSearchResult', function() {
     return {
-        templateUrl: '/views/search-results/search-results.html',
-        restrict: 'A'
+        templateUrl: '/views/search-results/search-results.html'
     };
 
 })
