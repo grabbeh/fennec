fff4ecda323621d538d14cfdd623407c5112a83b
Michael <mbg@outlook.com>
2014-09-23 16:41:03 +0100

    Update search-results.js

--- client/views/search-results/search-results.js
+++ client/views/search-results/search-results.js
@@ -1,7 +1,22 @@
 angular.module('app')
-.directive('mgSearchResult', function() {
-    return {
-        templateUrl: '/views/search-results/search-results.html'
-    };
+.directive('mgSearchResults', function() {
+      return {
+            replace: true, 
+            templateUrl: '/views/search-results/search-results.html',
+            scope: {
+                searchResults: '=',
+                showModal: '&'
+            },
+            controller: function($scope) {
+                var $ = $scope;
+                $.removeResults = function(){
+                    $.searchResults = false;
+                }
+                $.showModalWrapper = function(trademark){
+                    var func = $.showModal();
+                    func(trademark);
+                }
 
+          }
+      }
 })
