e16a0b4988ad620fa14e0239e76cd8073f39d2f2
Michael <mbg@outlook.com>
2014-10-22 17:20:28 +0100

    Update settings.js

--- client/views/settings/settings.js
+++ client/views/settings/settings.js
@@ -1,5 +1,6 @@
 angular.module('app')
     .controller('settingsCtrl', ['$scope', 'user', function($scope, user) {
+        console.log(user);
         var $ = $scope;
         $.user = user;
         }
