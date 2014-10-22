a8b992e52a2854dea0831b8b0e0ccaaf6c109c52
Michael <mbg@outlook.com>
2014-10-22 16:58:56 +0100

    Update year-selector.js

--- client/views/year-selector/year-selector.js
+++ client/views/year-selector/year-selector.js
@@ -1,1 +1,19 @@
-<div></div>
+angular.module('app')
+.directive('mgYearSelector', function() {
+    return {
+        replace: true,
+        templateUrl: '/views/year-selector/year-selector.html',
+        scope: {
+            expirySearch: '&'
+        },
+        controller: function($scope, $filter) {
+        
+            var $ = $scope;
+            $.years = $filter('createTen')(new Date().getFullYear());
+            $.expirySearchWrapper = function(obj) {
+                var func = $.expirySearch();
+                func(obj);
+            }
+        }
+    }
+})
