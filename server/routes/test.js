b1384bb9f531e492c561885df846646e94849320
Michael <mbg@outlook.com>
2014-10-23 10:53:37 +0100

    Update test.js

--- server/routes/test.js
+++ server/routes/test.js
@@ -1,4 +1,5 @@
 var helper = require('./helper')
+, Trademark = require('../models/trademarkSchema')
 , express = require('express')
 , db = require('../config/paid-db.js')
 , mongoose = require('mongoose')
@@ -8,7 +9,7 @@ var helper = require('./helper')
 mongoose.connect(db, function(err){
     if (err) { console.log(err); throw new Error(err.stack);}
   });
-
+/*
 var d = new Date();
 console.log("new Date")
 console.log(d);
@@ -18,23 +19,32 @@ console.log(j);
 console.log("Parsed date")
 console.log(JSON.parse(j));
 console.log("new Date + JSON parse")
-console.log(new Date(JSON.parse(j)));
+console.log(new Date(JSON.parse(j)));*/
 
+Trademark.find({entity: "ACME INC"}).lean().exec(function(err, trademarks){
+    trademarks.forEach(function(tm){
+        if (tm.filingDate.DDate && tm.filingDate.DDate !instanceof Date){
+            console.log("Not filing date object")
+        }
+        if (tm.registrationDate.DDate && tm.registrationDate.DDate !instanceof Date){
+            console.log("Not registration date object")
+        }
+        if (tm.expiryDate.DDate && tm.expiryDate.DDate !instanceof Date){
+            console.log("Not expiry date object")
+        }
+    })
+})
+/*
 app.get('/', function(req, res){
    
     // returns old format
     helper.getTrademark("53c8e75f730766d56dd6aba0", function(err, trademark){
         //console.log(trademark.registrationDate.DDate);
         //console.log(j);
-        
-
         res.json(trademark);
     });
-
-
-
 })
 
 app.listen(2001);
 console.log("Server listening on port 2001");
-
+*/
