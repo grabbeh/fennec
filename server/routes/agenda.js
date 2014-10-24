e19f278d2b9063cc6abf238b814f8da246838a9a
Michael <mbg@outlook.com>
2014-10-24 10:39:40 +0100

    Update agenda.js

--- server/routes/agenda.js
+++ server/routes/agenda.js
@@ -161,7 +161,7 @@ var trademarks = [
     stringDate: "3/22/2013"
   },
   expiryDate: {
-    stringDate: "9/20/2014"
+    stringDate: "9/25/2014"
   },
   applicationNumber: "3060238",
   registrationNumber: "2559727",
