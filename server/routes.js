785b64dfae3c1de73661148dfcd059d14dcb6ab7
Michael <mbg@outlook.com>
2014-11-05 12:51:47 +0000

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -106,6 +106,7 @@ module.exports = function(app){
 	// Invite mechanism
 	
 	app.post('/api/sendInvite', invite.sendInvite);
+	app.get('/server/invite/:id', invite.getInvite);
 	app.post('/server/invite/:id', invite.acceptInvite);
 
 	// Spreadsheet upload
