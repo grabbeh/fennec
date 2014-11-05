6406cf73dface29ac065febb7fa1ebe1743f244b
Michael <mbg@outlook.com>
2014-11-05 11:27:18 +0000

    Update invite.js

--- server/routes/invite.js
+++ server/routes/invite.js
@@ -1,1 +1,30 @@
-var invite = "invite";
+var Invite = require('../models/inviteSchema')
+, async = require('async')
+, email = require('./email')
+, html = require('./html')
+
+exports.createInvite = function(req, res){
+    var email = req.body.email;
+    async.auto({
+        invite: function(cb, results){
+            addInvite(email, cb)
+        },
+        html: ['invite', function(cb, results){
+            var fileLocation = path.resolve(__dirname, '../email-templates/invite.html');
+            html.returnHtml(results.invite, fileLocation, cb);
+        }],
+        sendEmail: ['html', function(cb, results){
+            email.sendEmail(id, "Invite to Fennec", results.html, cb)
+        }]
+        }, function(err, results){
+            res.json("Invite sent")
+    })     
+}
+
+function addInvite(email, fn){
+    new Invite({
+      email: email
+    }).save(function(err, invite){
+        return fn(null, invite)
+    })
+}
