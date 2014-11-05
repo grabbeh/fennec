bc443a866bed71e14d8cbb0b50e28d87614f650e
Michael <mbg@outlook.com>
2014-11-05 15:47:54 +0000

    Update accept-invite.js

--- client/views/accept-invite/accept-invite.js
+++ client/views/accept-invite/accept-invite.js
@@ -1,6 +1,6 @@
 angular.module('app')
-    .controller('acceptInviteCtrl', ['$scope', 'invite', 'notificationModal', 'userService', '$routeParams',
-    function($scope, invite, notificationModal, userService, $routeParams) {
+    .controller('acceptInviteCtrl', ['$scope', '$rootScope', '$location', '$window', 'invite', 'notificationModal', 'userService', '$routeParams',
+    function($scope, $rootScope, $location, $window, invite, notificationModal, userService, $routeParams) {
     var $ = $scope;
     $.invite = invite;
 
@@ -10,9 +10,13 @@ angular.module('app')
         else {
               userService.acceptInvite($.newUser, $routeParams.id)
                 .then(function(res) {
-                    console.log(res);
-                if (!res.error)
-                    notificationModal.activate({ success: res.success });
+
+                if (!res.error){
+                    notificationModal.activate({ success: "Congratulations, you've successfully created an account" });
+                    $window.sessionStorage.token = res.token;
+                    $rootScope.user = true;
+                    $location.path('/home');
+                }
                 else
                     notificationModal.activate({error: res.error});
               });
