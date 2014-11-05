65cc29351bc05b8254961bfdb56ef74444115977
Michael <mbg@outlook.com>
2014-11-05 11:10:51 +0000

    Update inviteSchema.js

--- server/models/inviteSchema.js
+++ server/models/inviteSchema.js
@@ -1,1 +1,10 @@
-var Schema = "schema";
+var mongoose = require('mongoose')
+, Schema = mongoose.Schema
+, ObjectId = Schema.ObjectId;
+
+var invite = new Schema({
+  active: { type: Boolean, default: true },
+  email: String
+});
+
+module.exports = mongoose.model('invite', invite);
