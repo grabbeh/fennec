406c492185364745fcda9580a8568f9a04749e89
Michael <mbg@outlook.com>
2015-02-09 17:54:30 +0000

    Update config.js

--- client/vendor/config.js
+++ client/vendor/config.js
@@ -1,2 +1,2 @@
-var app = angular.module('app',['ngRoute', 'ngTouch', 'btford.modal', 'angles', 'ngUpload','angular-momentjs', 'angular-datepicker']);
+var app = angular.module('app',['ngRoute', 'ngTouch', 'btford.modal', 'angles', 'ngUpload','angular-momentjs', 'pickadate']);
 
