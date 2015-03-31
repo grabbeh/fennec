1869fef64f2a23f887ad56e3b98d575868755267
Michael <mbg@outlook.com>
2015-03-31 23:29:06 +0100

    Update admin-favourites.js

--- client/views/admin-favourites/admin-favourites.js
+++ client/views/admin-favourites/admin-favourites.js
@@ -1,32 +1,28 @@
 angular.module('app')
 .directive('mgAdminFavourites', function() {
     return {
-        require: '^mgPaginator',
+        //require: '^mgPaginator',
         templateUrl: "/views/admin-favourites/admin-favourites.html",
         replace: true,
         scope: {
             paginatedMarks: '=',
-            items: '='//,
-            //showModal: '&'
-        },
+            items: '=',
+            showModal: '&'
+        }/*,
         link: function(scope, element, attrs, mgPaginatorCtrl){
             var $ = scope;
-            console.log(mgPaginatorCtrl.outerShowModal());
             $.innerShowModal = function(tm){
-                console.log(tm);
-                console.log(mgPaginatorCtrl);
                 var func = mgPaginatorCtrl.outerShowModal();
-                console.log(func);
                 func(tm);
             }
             
-        }/*,
+        }*/,
          controller: function($scope) {
             var $ = $scope;
             $.innerShowModal = function(tm) {
                 var func = $.showModal();
                 func(tm);
             }
-        }*/
+        }
     }
 })
