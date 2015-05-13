4d24837fcd6cd42a1826b49dc5783e1f0c091e79
Michael <mbg@outlook.com>
2015-05-13 14:29:23 +0100

    Update userSchema.js

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -3,80 +3,76 @@ var mongoose = require('mongoose')
    , ObjectId = Schema.ObjectId;
 
 var User = new Schema({
-	portfolios: Array,
+    portfolios: Array,
     entity: String,
     favourites: Array,
-	_id: String,
-	hash: String,
-	email: String,
-	alertFrequency: Array,
-	displayOptions: { type: Object, default: {
-    showFavourites:
-    {
-       showFavourites: true,
-       name: "Favourites",
-       checked: true
+    _id: String,
+    hash: String,
+    email: String,
+    alertFrequency: Array,
+    displayOptions: {
+        type: Object,
+        default: {
+            showFavourites: {
+                showFavourites: true,
+                name: "Favourites",
+                checked: true
+            },
+            showIncomplete: {
+                showIncomplete: true,
+                name: "Incomplete marks",
+                checked: true
+            },
+            showExpiring: {
+                showExpiring: true,
+                name: "Expiring marks",
+                checked: true
+            },
+            showActivity: {
+                showActivity: true,
+                name: "Activities",
+                checked: true
+            },
+            showMap: {
+                showMap: true,
+                name: "Map",
+                checked: true
+            },
+            showChart: {
+                showChart: true,
+                name: "Expiry chart",
+                checked: true
+            },
+            showSummary: {
+                showSummary: true,
+                name: "Summary",
+                checked: true
+            }
+
+        }
     },
-    showIncomplete:
-    {
-      showIncomplete: true,
-      name: "Incomplete marks",
-      checked: true
+    alertOptions: {
+        type: Array,
+        default: [{
+            checked: false,
+            name: "Send alert when trade mark expires",
+            functionName: "sendExpiredAlerts",
+            type: "recurring"
+        }, {
+            checked: false,
+            name: "Send reminders for expiry",
+            functionName: "sendExpiryAlerts",
+            type: "recurring"
+        }, {
+            checked: false,
+            name: "Send alert when trade mark added, edited, or deleted",
+            functionName: "sendAlertOnChange",
+            type: "one-off"
+        }]
     },
-    showExpiring: 
-    {
-      	showExpiring: true,
-      	name: "Expiring marks",
-      	checked: true
-    },
-    showActivity:
-    {
-      	showActivity: true,    
-      	name: "Activities",
-      	checked: true
-    },
-    showMap:
-    {
-      showMap: true,
-      name: "Map",
-          checked: true
-    },
-    showChart:
-    {
-      	showChart: true,
-      	name: "Expiry chart",
-      	    checked: true
-    }, 
-    showSummary:
-    {
-      	showSummary: true,
-      	name: "Summary",
-      	checked: true
+    isAdmin: {
+        type: Boolean,
+        default: false
     }
-
-		};
-	},
-	alertOptions: { type: Array, default:  [
-	    {
-	      checked: false,
-	      name: "Send alert when trade mark expires",
-	      functionName: "sendExpiredAlerts",
-	      type: "recurring"
-	    },
-	    {
-	      checked: false,
-	      name: "Send reminders for expiry",
-	      functionName: "sendExpiryAlerts",
-	      type: "recurring"
-	    },
-	    {
-	      checked: false,
-	      name: "Send alert when trade mark added, edited, or deleted",
-	      functionName: "sendAlertOnChange",
-	      type: "one-off"
-	    }
-	  ]},
-	isAdmin: {type: Boolean, default: false}
 });
-
 module.exports = mongoose.model('User', User); 
