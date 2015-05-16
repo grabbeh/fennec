fe8b10472df357b84763b05b5fa772729295b61a
Michael <mbg@outlook.com>
2015-05-17 00:18:49 +0100

    Added markdown capability to comments box

--- client/vendor/config.js
+++ client/vendor/config.js
@@ -1,2 +1,3 @@
-var app = angular.module('app',['ngRoute', 'ngAnimate', 'ngTouch', 'btford.modal', 'angles', 'ngFileUpload','angular-momentjs']);
+var app = angular.module('app',['ngRoute', 'ngAnimate', 'ngTouch', 'btford.modal', 'angles', 'ngFileUpload','angular-momentjs',  'ngSanitize',
+  'btford.markdown']);
 
