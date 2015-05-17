2c626e4850a9dfec4397f16bdeceb6900ca528b9
Michael <mbg@outlook.com>
2015-05-17 22:19:10 +0100

    Update search-results.js

--- client/views/search-results/search-results.js
+++ client/views/search-results/search-results.js
@@ -1,21 +1,19 @@
 angular.module('app')
 .directive('mgSearchResults', function() {
       return {
-            require: '^mgSearchPanel',
             replace: true, 
             templateUrl: '/views/search-results/search-results.html',
             scope: {
                 searchResults: '='
             },
-            link: function(scope, element, attrs, mgSearchPanelCtrl) {
-                 scope.showModalWrapper = function(trademark){
-                    var func = mgSearchPanelCtrl.showModal(trademark);
-                    func(trademark);
-                }
-            },
-            controller: function($scope) {
+            controller: function($scope, trademarkModal) {
                 $scope.removeResults = function(){
                     $scope.searchResults = false;
+                }
+                
+                $.showModal = function(trademark) {
+                    trademarkModal.deactivate();
+                    trademarkModal.activate({ trademark: trademark })
                 }
                
 
