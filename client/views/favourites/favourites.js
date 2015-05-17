d2ccc33fe813f53be334cfe7fa6b844ea366dad7
Michael <mbg@outlook.com>
2015-05-17 15:15:06 +0100

    Added modal fn

--- client/views/favourites/favourites.js
+++ client/views/favourites/favourites.js
@@ -14,6 +14,14 @@ angular.module('app')
             $.activeTrademark = trademark;
         };
 
+$.showModal = function(trademark) {
+                $rootScope.modal = true;
+                trademarkModal.deactivate();
+                trademarkModal.activate({
+                    trademark: trademark,
+                    user: user
+                });
+            };
         $.openEditTrademarkModal = function(trademark) {
             editTrademarkModal.activate({
                 trademark: trademark
