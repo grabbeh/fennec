42c439912f226b1e574f1730972ea2f2d5c711bd
Michael <mbg@outlook.com>
2015-05-18 10:11:33 +0100

    Update view-country.js

--- client/views/view-country/view-country.js
+++ client/views/view-country/view-country.js
@@ -51,7 +51,7 @@ angular.module('app')
             trademarkModal.activate({
                 trademark: trademark,
                 user: user
-            });
+            }, { broadcast: true });
         };
 
         $.sendMarksToServer = function(marks) {
