abbe554977c0f076eff6dc289d0f1f7ee0460262
Michael <mbg@outlook.com>
2015-05-19 11:36:53 +0100

    Update admin-favourities-no-modal.js

--- client/views/admin-favourites-no-modal/admin-favourities-no-modal.js
+++ client/views/admin-favourites-no-modal/admin-favourities-no-modal.js
@@ -1,1 +1,21 @@
-
+angular.module('app')
+.directive('mgAdminFavourites', function() {
+    return {
+        require: '^mgPaginator',
+        templateUrl: "/views/admin-favourites/admin-favourites.html",
+        replace: true,
+        scope: {
+            items: '=',
+            paginatedMarks:'=',
+            activeTrademark: '='
+        },
+        link: function(scope, elem, attrs){
+        },
+         controller: function($scope) {
+            var $ = $scope;
+            $.activateTrademark = function(trademark) {
+              $.activeTrademark = trademark;
+            };
+        }
+    }
+})
