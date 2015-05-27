196119a096fcea231bf1ca4ea19e692d348da010
Michael <mbg@outlook.com>
2015-05-27 14:59:51 +0100

    Update favourite-button.js

--- client/views/favourite-button/favourite-button.js
+++ client/views/favourite-button/favourite-button.js
@@ -13,15 +13,19 @@ angular.module('app')
         controller: function($scope, userService) {
             var $ = $scope;
             $.toggleFavourite = function() {
+                console.log($.trademark);
                 if ($.trademark.favourite) {
                     $.trademark.favourite = false;
                     $.user.favourites.forEach(function(fav, i) {
-                        if (fav === $.trademark._id) 
+                        if (fav === $.trademark._id) {
+                            console.log("Favourite removed from user array");
                             $.user.favourites.splice(i, 1);
+                        }
                     })
                     userService.updateUser($.user).then(function(res) {});
 
                 } else {
+                    console.log("Not previously favourited");
                     $.trademark.favourite = true;
                     $.user.favourites.push($.trademark._id)
                     userService.updateUser($.user).then(function(res) {})
