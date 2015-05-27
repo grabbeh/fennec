52cdcfa4d7d3b92cab45d387053c6150ce6af360
Michael <mbg@outlook.com>
2015-05-27 15:07:52 +0100

    Update view-trademark.js

--- client/views/view-trademark/view-trademark.js
+++ client/views/view-trademark/view-trademark.js
@@ -1,23 +1,10 @@
 angular.module('app')
 
-.controller('trademarkViewCtrl', ['$scope', '$rootScope', '$routeParams', 'trademark', 'trademarkService', 'user', 'editTrademarkModal',
-    function($scope, $rootScope, $routeParams, trademark, trademarkService, user, editTrademarkModal) {
+.controller('trademarkViewCtrl', ['$scope', '$rootScope', '$routeParams', 'trademark', 'user',
+    function($scope, $rootScope, $routeParams, trademark,  user) {
         var $ = $scope;
         $.activePortfolio = $routeParams.portfolio;
         $.trademark = trademark;
         $.user = user;
-        $.openEditTrademarkModal = function(trademark) {
-            editTrademarkModal.activate({
-                trademark: trademark
-            });
-            $rootScope.modal = true;
-        };
-
-        $.deleteTrademark = function() {
-            trademarkService.deleteMark($.trademark)
-                .success(function(data) {
-                    $scope.message = data.message;
-                });
-        };
     }
 ])
