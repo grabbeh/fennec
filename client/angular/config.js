3f51880a36161ec08bfb2993353c86db1bd24af0
Michael <mbg@outlook.com>
2015-05-28 15:29:08 +0100

    Create config.js

---
+++ client/angular/config.js
@@ -0,0 +1,13 @@
+var angular = require('angular');
+
+var app = 
+angular.module('app',[
+		require('angular-route'), 
+		require('angular-animate'),
+		require('angular-touch'),
+		'ngFileUpload',
+		'angular-momentjs',  
+		'ngSanitize',
+		'angles', 
+		'btford.modal',
+		'btford.markdown']);
