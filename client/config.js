f5c5fc91bf182f4d463fb590361d5b295deb2d65
Michael <mbg@outlook.com>
2015-05-29 11:28:22 +0100

    Update config.js

--- client/config.js
+++ client/config.js
@@ -5,11 +5,11 @@ var L = require('leaflet')
 var app = 
 angular.module('app',[
 		require('angular-route'), 
-		require('angular-animate'),
-		require('angular-touch'),
+ 		require('angular-animate'),
+ 		require('angular-touch'),
 		require('angular-sanitize'),
-		require('./vendor/upload.js'),
-		require('./vendor/angular-moment.js'),  
-		require('./vendor/angles.js'), 
-		require('./vendor/modal.min.js'),
-		'btford.markdown'])
+ 		'ngFileUpload',
+ 		'angular-momentjs',  
+ 		'angles', 
+ 		'btford.modal',
+ 		'btford.markdown']);
