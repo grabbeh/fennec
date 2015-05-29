0858c8d629928e738a416829e512c859eedd0388
Michael <mbg@outlook.com>
2015-05-29 15:18:15 +0100

    Update search-input.js

--- client/views/search-input/search-input.js
+++ client/views/search-input/search-input.js
@@ -9,6 +9,7 @@ angular.module('app')
         controller: function($scope, $http, notificationModal) {
             var $ = $scope;
             $.search = function() {
+                console.log($.query);
                 if ($.query === "")
                     notificationModal.activate({ error: "Please provide a search term"}, { time: 2 })
                 $http.post('/api/search', { query: $.query })
