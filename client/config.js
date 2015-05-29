f6c41d9dec709547a4d43b359fa0746c84d9c20d
Michael <mbg@outlook.com>
2015-05-29 11:22:47 +0100

    Update config.js

--- client/config.js
+++ client/config.js
@@ -8,8 +8,8 @@ angular.module('app',[
 		require('angular-animate'),
 		require('angular-touch'),
 		require('angular-sanitize'),
-		require('vendor/upload.js'),
-		require('vendor/angular-moment.js'),  
-		require('vendor/angles.js'), 
-		require('vendor/modal.min.js'),
+		require('client/vendor/upload.js'),
+		require('client/vendor/angular-moment.js'),  
+		require('client/vendor/angles.js'), 
+		require('client/vendor/modal.min.js'),
 		'btford.markdown'])
