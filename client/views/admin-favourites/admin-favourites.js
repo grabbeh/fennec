11bf0efdd46790d8b52853d12cca9968f58d2577
Michael <mbg@outlook.com>
2015-07-02 17:09:20 +0100

    Update admin-favourites.js

--- client/views/admin-favourites/admin-favourites.js
+++ client/views/admin-favourites/admin-favourites.js
@@ -5,10 +5,11 @@ angular.module('app')
         templateUrl: "/views/admin-favourites/admin-favourites.html",
         replace: true,
         scope: {
-            items: '=',
-            paginatedMarks:'='
+            items: '='
+            //paginatedMarks:'='
         },
         link: function(scope, elem, attrs){
+            console.log(scope);
         },
          controller: function($scope, trademarkModal) {
             var $ = $scope;
