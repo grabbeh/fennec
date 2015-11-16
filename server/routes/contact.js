48333fbe61d7f72a1535010056feed2de284ee47
Michael <mbg@outlook.com>
2015-11-16 13:01:37 +0000

    Update contact.js

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -6,7 +6,7 @@ exports.processMessage = function(req, res){
     var fileLocation = path.resolve(__dirname, '../email-templates/contact-message.html')
     html.returnHtml(req.body.msg, fileLocation, function(err, contents){
             email.sendEmail('mbg@outlook.com', 'New contact', contents, function(err, json){
-        		res.send({ msg: "Thank you - we'll be in touch"})
+        		res.send();
    		})
     })
 }
