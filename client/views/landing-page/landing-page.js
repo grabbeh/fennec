6190662501548a1cbfc9b17f1845321761bfcb35
Michael <mbg@outlook.com>
2015-11-19 18:13:50 +0000

    Update landing-page.js

--- client/views/landing-page/landing-page.js
+++ client/views/landing-page/landing-page.js
@@ -4,13 +4,11 @@ angular.module('app')
     function($scope, $window, $http, notificationModal, userService, $location, $rootScope) {
         var $ = $scope;
         $.loadDemo = function() {
-            console.log("Button clicked");
             userService.logIn({
                     password: "demo",
                     email: "demo@demo.com"
                 })
                 .then(function(res) {
-                    console.log("Data returned");
                     $window.sessionStorage.token = res.data.token;
                     $rootScope.user = true;
                     $location.path('/home');
