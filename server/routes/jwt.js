c9c8f7071a169520f5e4d813fd87771fbee60fbb
Michael <mbg@outlook.com>
2016-03-16 14:31:01 +0000

    Update jwt.js

--- server/routes/jwt.js
+++ server/routes/jwt.js
@@ -2,14 +2,15 @@ var jwt = require('jsonwebtoken')
 , secret = require('../config/jwt-secret')
 
 exports.createToken = function(user, fn){
-
-   
-    var payload = {};
+   var payload = {};
    payload._id = user._id;
    payload.entity = user.entity;
    payload.isAdmin = user.isAdmin;
    console.log(payload);
+   console.log(secret);
    jwt.sign(payload, secret, {}, function(err, token){
+   	console.log(err);
+   	console.log(token);
     	if (err) { console.log("Error with token creation"); console.log(err); return fn(err)}
     	console.log("Token returned");
     	console.log(token);
